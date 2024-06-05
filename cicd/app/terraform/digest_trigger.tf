a6d7dbb724d41d113efc1c3d13fc279292d21d7d
Steve Kehlet <steve.kehlet@avaneerhealth.com>
2024-06-05 11:26:57 -0700

    adjust for UTC

--- cicd/app/terraform/digest_trigger.tf
+++ cicd/app/terraform/digest_trigger.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "twice_daily" {
   name                = "${local.app_id}-twice-daily"
   description         = "Fires twice daily"
-  schedule_expression = "cron(0 6,14 * * ? *)"
+  schedule_expression = "cron(0 13,21 * * ? *)" # times are in UTC
 }
 
 resource "aws_cloudwatch_event_target" "invoke_digest_twice_daily" {
--- cicd/app/terraform/rss_reader_trigger.tf
+++ cicd/app/terraform/rss_reader_trigger.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "twice_daily_prior_to_digest" {
   name                = "${local.app_id}-twice-daily-prior-to-digest"
   description         = "Fires twice daily, just prior to the Digest lambda running"
-  schedule_expression = "cron(30 5,13 * * ? *)"
+  schedule_expression = "cron(30 12,20 * * ? *)" # times are in UTC
 }
 
 resource "aws_cloudwatch_event_target" "invoke_rss_reader_twice_daily" {
