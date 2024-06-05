28148bb7a5406b92761b96de0bc544f4cfbdbe51
Steve Kehlet <steve.kehlet@avaneerhealth.com>
2024-06-05 13:44:11 -0700

    testing cwe triggers

--- cicd/app/terraform/rss_reader_trigger.tf
+++ cicd/app/terraform/rss_reader_trigger.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_event_rule" "twice_daily_prior_to_digest" {
   name                = "${local.app_id}-twice-daily-prior-to-digest"
   description         = "Fires twice daily, just prior to the Digest lambda running"
   # schedule_expression = "cron(30 12,20 * * ? *)" # times are in UTC
-  schedule_expression = "cron(40 20 * * ? *)" # times are in UTC
+  schedule_expression = "cron(50 20 * * ? *)" # times are in UTC
 }
 
 resource "aws_cloudwatch_event_target" "invoke_rss_reader_twice_daily" {
