2a3c0c81a2fdbbb913e3a3d3c47117957574b870
Steve Kehlet <steve.kehlet@avaneerhealth.com>
2024-06-06 19:17:53 -0700

    bookmarklet feedback

--- src/link_reader/misc/bookmarklet.js
+++ src/link_reader/misc/bookmarklet.js
@@ -7,5 +7,18 @@ javascript: (function () {
         'Authorization': 'Basic base64-encoded-credentials-here',
         'Content-Type': 'text/plain',
       },
-    })
+    }).then(res => res.json()).then(data => {
+      const summary = data.result;
+      const banner = document.createElement('div');
+      banner.style.position = 'fixed';
+      banner.style.left = '0';
+      banner.style.right = '0';
+      banner.style.top = '0';
+      banner.style.zIndex = 999999999;
+      banner.style.background = '#000';
+      banner.style.color = '#fff';
+      banner.style.padding = '15px';
+      banner.innerHTML = summary;
+      document.body.appendChild(banner);
+    });
 })();
