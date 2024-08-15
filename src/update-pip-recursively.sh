0a4bf840910c8276fa655b2615e5b3496bd5a1d0
Steve Kehlet <steve.kehlet@avaneerhealth.com>
2024-08-15 10:36:06 -0700

    add pydantic to digest; improve pip upgrade script; upgrade libs

--- src/update-pip-recursively.sh
+++ src/update-pip-recursively.sh
@@ -11,11 +11,19 @@ do
     if [[ -f $dir/requirements.txt ]]; then
         echo $dir
         cd $dir
+        cp -p requirements.txt requirements.orig.txt
         gsed -i -e 's/==.*//' requirements.txt
         python3 -m venv .venv
         source .venv/bin/activate
         pip install -r requirements.txt
-        pip freeze > requirements.txt
+        rm -f requirements.new.txt
+        for package in $(cat requirements.txt)
+        do
+            pip freeze | grep "^${package}=" >> requirements.new.txt
+        done
+        diff requirements.orig.txt requirements.new.txt || :
+        mv requirements.new.txt requirements.txt
+        rm -f requirements.orig.txt
         deactivate
         rm -rf .venv
         cd ..
