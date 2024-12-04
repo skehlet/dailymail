5500ec0bbb5b4c289e467da8c829e1f08aff2781
Steve Kehlet <steve.kehlet@avaneerhealth.com>
2024-12-04 15:04:02 -0800

    switch to just once a day

--- cicd/app/terraform/digest_cloudwatch_event.tf
+++ cicd/app/terraform/digest_cloudwatch_event.tf
@@ -1,10 +1,10 @@
-resource "aws_cloudwatch_event_rule" "twice_daily" {
-  name                = "${local.app_id}-twice-daily"
-  description         = "Fires twice daily"
-  schedule_expression = "cron(0 14,21 * * ? *)" # times are in UTC
+resource "aws_cloudwatch_event_rule" "digest" {
+  name                = "${local.app_id}-digest"
+  description         = "Fires in the afternoon (PT)"
+  schedule_expression = "cron(0 21 * * ? *)" # times are in UTC
 }
 
 resource "aws_cloudwatch_event_target" "invoke_digest_twice_daily" {
-  rule = aws_cloudwatch_event_rule.twice_daily.name
+  rule = aws_cloudwatch_event_rule.digest.name
   arn  = aws_lambda_function.digest.arn
 }
--- cicd/app/terraform/rss_reader_cloudwatch_event.tf
+++ cicd/app/terraform/rss_reader_cloudwatch_event.tf
@@ -1,10 +1,10 @@
-resource "aws_cloudwatch_event_rule" "twice_daily_prior_to_digest" {
-  name                = "${local.app_id}-twice-daily-prior-to-digest"
-  description         = "Fires twice daily, a short while before the Digest lambda runs"
-  schedule_expression = "cron(30 13,20 * * ? *)" # times are in UTC
+resource "aws_cloudwatch_event_rule" "before_digest" {
+  name                = "${local.app_id}-before-digest"
+  description         = "Fires in the afternoon (PT), a short while before the Digest lambda runs"
+  schedule_expression = "cron(30 20 * * ? *)" # times are in UTC
 }
 
 resource "aws_cloudwatch_event_target" "invoke_rss_reader_twice_daily" {
-  rule = aws_cloudwatch_event_rule.twice_daily_prior_to_digest.name
+  rule = aws_cloudwatch_event_rule.before_digest.name
   arn  = aws_lambda_function.rss_reader.arn
 }
